8abbb13732cba761b3c03dee2328ed2f2ab6a082
AldsDeveloper <abdulloh.mukemm@gmail.com>
2024-12-15 02:17:09 +0700

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
-import logger from 'jet-logger';
-
 import Env from '@src/common/Env';
+import logger from 'jet-logger';
 import server from './server';
+import { sequelize } from './config/database.config';
 
-
-// **** Run **** //
-
-const SERVER_START_MSG = ('Express server started on port: ' + 
-  Env.Port.toString());
+const SERVER_START_MSG =
+  'Express server started on port: ' + Env.Port.toString();
 
 server.listen(Env.Port, () => logger.info(SERVER_START_MSG));
+
+const PORT = process.env.PORT || 4000;
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connected");
+
+    server.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
