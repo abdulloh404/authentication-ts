b9be38140390cc52890daa45b27f0d043bceb79e
AldsDeveloper <abdulloh.mukemm@gmail.com>
2024-12-15 03:34:29 +0700

    Initial commit

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  port: process.env.PORT || 3000, // เปลี่ยนค่าเริ่มต้นเป็น 3000
+  port: process.env.PORT || 3000,
   host: process.env.HOST || 'localhost',
   jwtSecret: process.env.JWT_SECRET || '',
   mysql: {
@@ -10,9 +10,8 @@ export const config = {
     user: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_PASSWORD || 'frankent',
     database: process.env.MYSQL_DATABASE || 'authentication',
-    port: parseInt(process.env.MYSQL_PORT || '3306'), // ใช้ 3306 เป็นค่าพอร์ตเริ่มต้น
+    port: parseInt(process.env.MYSQL_PORT || '3306'),
   },
-
   emailService: process.env.EMAIL_SERVICE,
   emailUser: process.env.EMAIL_USER,
   emailPass: process.env.EMAIL_PASS,
