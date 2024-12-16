fba6f70115e65edcb347d4e6409d1e3441070546
Abdulloh <abdulloh.mukemm@gmail.com>
2024-12-16 13:02:46 +0700

    feat(campaign!):✗ handle migration-users

--- src/middleware/auth.handler.ts
+++ src/middleware/auth.handler.ts
@@ -1,20 +1,30 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
-const authenticate = (req: Request, res: Response, next: NextFunction) => {
+export const authenticate = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const token = req.headers.authorization?.split(' ')[1];
+
   if (!token) {
-    return res.status(401).json({ error: 'No token provided' });
+    return res.status(401).json({ error: 'Access token is required' });
   }
 
   try {
     const decoded = jwt.verify(
       token,
-      process.env.JWT_SECRET || 'your_secret_key'
-    );
-    // req.body.userId = decoded.id;
-    next();
+      process.env.JWT_SECRET || ''
+    ) as JwtPayload;
+
+    if (decoded && typeof decoded === 'object' && 'id' in decoded) {
+      req.body.userId = decoded.id;
+      next();
+    } else {
+      return res.status(403).json({ error: 'Invalid token payload' });
+    }
   } catch (err) {
-    res.status(403).json({ error: 'Invalid or expired token' });
+    return res.status(403).json({ error: 'Invalid or expired token' });
   }
 };
