e388cdaad4f5a0c7e0a2c6efcbf847dd9380f907
AldsDeveloper <abdulloh.mukemm@gmail.com>
2024-12-18 00:24:20 +0700

    feat(package!):nx setting

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,18 @@ import server from './server';
 import { sequelize } from './config/database.config';
 
 const SERVER_START_MSG =
-  
-  
-  
-  
   'Express server started on port: ' + Env.Port.toString();
 
 const SERVER_START_MY_SQL =
-  dsadsa
-  
-  
-  'Database connected succesfdsadsaully started with port :' + process.env.MYSQL_PORT;
+  'Database connected succesfully started with port :' + process.env.MYSQL_PORT;
 
+
+
+
+  
 (async () => {
   try {
     await sequelize.authenticate();
-
     logger.info(SERVER_START_MY_SQL);
 
     server.listen(Env.Port, () => {
