74f20ea78e8a9ba954e40fbfa0de82d905e276c8
AldsDeveloper <abdulloh.mukemm@gmail.com>
2024-12-18 01:03:25 +0700

    feat(package!):nx setting

--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,11 @@ import logger from 'jet-logger';
 
 import server from './server';
 
-
-
-
-
-
-
-
-
-
-
-
-
 import { sequelize } from './config/database.config';
 
 const SERVER_START_MSG =
   'Express server started on port: ' + Env.Port.toString();
 
-
-  
-
-
-
-  
 const SERVER_START_MY_SQL =
   'Database connected succesfully started with port :' + process.env.MYSQL_PORT;
 
