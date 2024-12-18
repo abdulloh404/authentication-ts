ed55a44bc0387915209bc9275b8f885cc435f63f
AldsDeveloper <abdulloh.mukemm@gmail.com>
2024-12-18 21:46:40 +0700

    feat(package!):setting models sqlize

---
+++ migrations/20241218144343-create-personal-access-token.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('PersonalAccessTokens', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      tokenable_type: {
+        type: Sequelize.STRING,
+      },
+      tokenable_id: {
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      token: {
+        type: Sequelize.STRING,
+      },
+      abilities: {
+        type: Sequelize.TEXT,
+      },
+      last_used_at: {
+        type: Sequelize.DATE,
+      },
+      expires_at: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('PersonalAccessTokens');
+  },
+};
