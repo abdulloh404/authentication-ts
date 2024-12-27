07a78f34594d5d78b2752b806dd26114ce76cb69
Abdulloh404 <abdulloh.mukemm@gmail.com>
2024-12-28 05:45:59 +0700

    feat(structors):✗eslint --setiing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,12 @@
-import { configs } from '@nx/eslint-plugin';
+import nx from '@nx/eslint-plugin';
+const { configs } = nx;
 
 export default [
   ...configs['flat/base'],
   ...configs['flat/typescript'],
   ...configs['flat/javascript'],
   {
-    ignores: ['**/dist'],
+    ignores: ['**/dist', 'temp/*'],
   },
   {
     files: [
@@ -21,43 +22,9 @@ export default [
       '@typescript-eslint/no-misused-promises': 0,
       '@typescript-eslint/no-confusing-void-expression': 0,
       '@typescript-eslint/no-unnecessary-condition': 0,
-      '@typescript-eslint/restrict-template-expressions': [
-        'error',
-        { allowNumber: true },
-      ],
-      '@typescript-eslint/restrict-plus-operands': [
-        'warn',
-        { allowNumberAndString: true },
-      ],
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/no-unsafe-enum-comparison': 0,
       '@typescript-eslint/no-unnecessary-type-parameters': 0,
-      '@stylistic/ts/semi': ['warn', 'always'],
-      '@stylistic/ts/member-delimiter-style': [
-        'warn',
-        {
-          multiline: {
-            delimiter: 'comma',
-            requireLast: true,
-          },
-          singleline: {
-            delimiter: 'comma',
-            requireLast: false,
-          },
-          overrides: {
-            interface: {
-              singleline: {
-                delimiter: 'semi',
-                requireLast: false,
-              },
-              multiline: {
-                delimiter: 'semi',
-                requireLast: true,
-              },
-            },
-          },
-        },
-      ],
       '@typescript-eslint/no-non-null-assertion': 0,
       '@typescript-eslint/no-unused-expressions': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
@@ -67,9 +34,6 @@ export default [
       'no-extra-boolean-cast': 0,
       indent: ['warn', 2],
       quotes: ['warn', 'single'],
-      'n/no-process-env': 1,
-      'n/no-missing-import': 0,
-      'n/no-unpublished-import': 0,
       'prefer-const': 'warn',
     },
   },
