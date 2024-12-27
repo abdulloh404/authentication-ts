91fd72ba753244e9e883494df7d9821e4eb5f1fb
Abdulloh404 <abdulloh.mukemm@gmail.com>
2024-12-28 06:53:48 +0700

    feat(structors):✗eslint --setiing

--- src/handler/async.handler.ts
+++ src/handler/async.handler.ts
@@ -1,24 +1,24 @@
+/* eslint-disable indent */
 import { HttpResponse } from '@src/common/HttpResponse';
 import { Request, Response, NextFunction } from 'express';
+import { ParamsDictionary } from 'express-serve-static-core';
+import { IncomingHttpHeaders } from 'http';
+import QueryString from 'qs';
+
+interface AsyncHandler {
+  headers: IncomingHttpHeaders;
+  params: ParamsDictionary;
+  query: QueryString.ParsedQs;
+  ip: string | undefined;
+  userAgent: string | undefined;
+}
 
 export const async =
-  (
-    handler: (requestBody: any, requestData: any) => Promise<HttpResponse>,
-    middleware?: (
-      req: Request,
-      res: Response,
-      next: NextFunction,
-    ) => void | Promise<void>,
-  ) =>
+  (handler: (requestBody: any, requestData: any) => Promise<HttpResponse>) =>
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      if (middleware) {
-        await middleware(req, res, next);
-      }
-
       const requestBody = req.body;
-
-      const requestData = {
+      const requestData: AsyncHandler = {
         headers: req.headers,
         params: req.params,
         query: req.query,
