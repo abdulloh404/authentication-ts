e420830213eb41ed9f4b32e5e68a7507148fcf8b
Abdulloh404 <abdulloh.mukemm@gmail.com>
2024-12-29 00:11:12 +0700

    feat(sequalize): @sequalize migrate db

--- migrations/20241218144122-create-personal-access-tokens.js
+++ migrations/20241218144122-create-personal-access-tokens.js
@@ -1,59 +1,56 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('personal_access_tokens', {
-      id: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      tokenable_type: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      tokenable_id: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        allowNull: false,
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      token: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      abilities: {
-        type: Sequelize.TEXT,
-        allowNull: true,
-      },
-      last_used_at: {
-        type: Sequelize.DATE,
-        allowNull: true,
-      },
-      expires_at: {
-        type: Sequelize.DATE,
-        allowNull: true,
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal(
-          'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
-        ),
-      },
-    });
-  },
-
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('personal_access_tokens');
-  },
-};
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable('personal_access_tokens', {
+    id: {
+      type: Sequelize.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    tokenable_type: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    tokenable_id: {
+      type: Sequelize.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    token: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    abilities: {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    },
+    last_used_at: {
+      type: Sequelize.DATE,
+      allowNull: true,
+    },
+    expires_at: {
+      type: Sequelize.DATE,
+      allowNull: true,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal(
+        'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
+      ),
+    },
+  });
+}
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.dropTable('personal_access_tokens');
+}
