daf2b6c1b955c7115d3b7b7645702e6b30ca5aed
Abdulloh404 <abdulloh.mukemm@gmail.com>
2024-12-31 11:23:03 +0700

    feat(nginx): https server config

--- src/common/Env.ts
+++ src/common/Env.ts
@@ -3,7 +3,8 @@ import { isEnumVal } from 'jet-validators';
 import { NodeEnvs } from './constants';
 import { string } from 'joi';
 
-// console.log('PORT in process.env:', process.env);
+console.log('PORT in process.env:', process.env);
+
 export default jetEnv({
   NodeEnv: isEnumVal(NodeEnvs),
   Host: string,
