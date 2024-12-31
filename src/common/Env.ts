266e9cef7b80ce1e408b3f21756045076e63adb2
Abdulloh404 <abdulloh.mukemm@gmail.com>
2024-12-31 11:32:38 +0700

    feat(nginx): https server config

--- src/common/Env.ts
+++ src/common/Env.ts
@@ -1,13 +1,11 @@
-import jetEnv, { num } from 'jet-env';
+import jetEnv, { num, str } from 'jet-env';
 import { isEnumVal } from 'jet-validators';
 import { NodeEnvs } from './constants';
-import { string } from 'joi';
 
 console.log('PORT in process.env:', process.env);
 
 export default jetEnv({
   NodeEnv: isEnumVal(NodeEnvs),
-  Host: string,
+  Host: str,
   Port: num,
-  MysqlPort: num,
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import './config/dotenv.config';
 import Env from '@src/common/Env';
 import fs from 'fs';
 import https from 'https';
@@ -5,24 +6,20 @@ import logger from 'jet-logger';
 import path from 'path';
 import server from './server';
 import { sequelize } from '@config/database.config';
-import './config/dotenv.config';
 
 /* eslint-disable no-console */
 
 const SERVER_ENV_MSG = `Loading .env from: ${path.join('config/.env.' + Env.NodeEnv)}`;
 const SERVER_START_MSG = `Express server started on: https://${Env.Host}:${Env.Port}`;
-const SERVER_START_MY_SQL = `Database connected successfully on port: ${Env.MysqlPort}`;
+const SERVER_START_MY_SQL = `Database connected successfully on port: ${3000}`;
 
 // SSL options
 const sslOptions = {
   key: fs.readFileSync(
-    path.resolve(
-      __dirname,
-      '../environments/certificate/localhost.com-key.pem',
-    ),
+    path.resolve('environments/certificate/localhost.com-key.pem'),
   ),
   cert: fs.readFileSync(
-    path.resolve(__dirname, '../environments/certificate/localhost.com.pem'),
+    path.resolve('environments/certificate/localhost.com.pem'),
   ),
 };
 
