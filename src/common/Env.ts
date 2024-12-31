e209e18206cb08d1d51ba1422311f819f3cd936c
Abdulloh404 <abdulloh.mukemm@gmail.com>
2024-12-31 12:27:20 +0700

    feat(nginx): https server config

--- src/common/Env.ts
+++ src/common/Env.ts
@@ -2,10 +2,16 @@ import jetEnv, { num, str } from 'jet-env';
 import { isEnumVal } from 'jet-validators';
 import { NodeEnvs } from './constants';
 
+// eslint-disable-next-line no-console
 console.log('PORT in process.env:', process.env);
 
 export default jetEnv({
   NodeEnv: isEnumVal(NodeEnvs),
   Host: str,
   Port: num,
+  MysqlHost: str,
+  MysqlPort: num,
+  MysqlUser: str,
+  MysqlPassword: str,
+  MysqlDatabase: str,
 });
