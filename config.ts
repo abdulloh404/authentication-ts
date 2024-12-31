fad42444fcf714d2c87adaa6c1a730591f139ec2
Abdulloh404 <abdulloh.mukemm@gmail.com>
2024-12-31 12:28:21 +0700

    feat(nginx): https server config

--- config.ts
+++ config.ts
@@ -1,11 +1,9 @@
-/* eslint-disable n/no-process-env */
-
 import path from 'path';
 import dotenv from 'dotenv';
 import moduleAlias from 'module-alias';
 
 // Check the env
-const NODE_ENV = process.env.NODE_ENV ?? 'development';
+const NODE_ENV = process.env['NODE_ENV'] ?? 'development';
 
 // Configure "dotenv"
 const result2 = dotenv.config({
