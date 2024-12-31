0006e559c065a1dd03b25b5b3e3a706f31fe4c6e
Abdulloh404 <abdulloh.mukemm@gmail.com>
2024-12-31 13:07:17 +0700

    feat(nginx): https server config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -7,7 +7,7 @@ export const sequelize = new Sequelize(
   config.mysql.password || '',
   {
     host: config.mysql.host || 'localhost',
-    port: parseInt(config.mysql.port ?? '3306', 10),
+    port: parseInt(config.mysql.port ?? '3306'),
     dialect: 'mysql',
     logging: true,
   },
