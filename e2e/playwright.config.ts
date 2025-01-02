d7486edbd90ee956357e97da8cf1624b6345e4ae
Abdulloh <abdulloh.mukemm@gmail.com>
2025-01-02 18:13:33 +0700

    feat(play-write):init playwrite install

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from '@playwright/test';
+import Env from '@src/common/Env';
 
 export default defineConfig({
   testDir: './tests',
   timeout: 30000,
   retries: 2,
   use: {
-    baseURL: process.env['CI'],
+    baseURL: Env.baseUrl,
     headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
