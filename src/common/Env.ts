2f499a94f2cea4f5bd7c2bbfd7ef9289e6ba5fd6
Abdulloh404 <abdulloh.mukemm@gmail.com>
2025-01-02 20:48:50 +0700

    feat(eslint): pull main branchs

--- src/common/Env.ts
+++ src/common/Env.ts
@@ -1,6 +1,9 @@
+import dotenv from 'dotenv';
 import jetEnv, { num, str } from 'jet-env';
 import { isEnumVal } from 'jet-validators';
 import { NodeEnvs } from './constants';
+
+dotenv.config();
 
 // eslint-disable-next-line no-console
 console.log('PORT in process.env:', process.env);
