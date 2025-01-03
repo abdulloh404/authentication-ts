300f90a1534ed9172a9036e8ced2ddd54d0aaa0d
Abdulloh <abdulloh.mukemm@gmail.com>
2025-01-03 10:48:10 +0700

    feat(play-write):init playwrite install

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,23 +1,41 @@
-import { defineConfig } from '@playwright/test';
 import dotenv from 'dotenv';
+import { defineConfig } from '@playwright/test';
+import { workspaceRoot } from '@nx/devkit';
 
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
 dotenv.config();
 
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+const baseURL = process.env['BASE_URL'];
+
 export default defineConfig({
-  testDir: './tests',
+  testDir: './src',
   timeout: 30000,
   retries: 2,
   use: {
-    baseURL: process.env['BASE_URL'],
+    baseURL: baseURL,
     headless: true,
+    ignoreHTTPSErrors: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
+    clientCertificates: [
+      {
+        origin: 'https://localhost.com:3000',
+        certPath: './../environments/certificate/localhost.com.crt',
+        keyPath: './../environments/certificate/localhost.com-key.pem',
+      },
+    ],
   },
-  projects: [
-    {
-      name: 'Chromium',
-      use: { ...process.env, browserName: 'chromium' },
-    },
-  ],
+  webServer: {
+    command: 'npx nx run serve',
+    url: 'https://localhost.com:3000',
+    reuseExistingServer: !process.env['CI'],
+    cwd: workspaceRoot,
+  },
   reporter: [['list'], ['html', { outputFolder: 'test-results' }]],
 });
