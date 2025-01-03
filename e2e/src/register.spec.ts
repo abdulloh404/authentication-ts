7459b978ef8d37bd256519efd26ac32190964c85
Abdulloh <abdulloh.mukemm@gmail.com>
2025-01-03 13:22:03 +0700

    feat(play-write):init playwrite install

--- e2e/src/register.spec.ts
+++ e2e/src/register.spec.ts
@@ -6,25 +6,29 @@ test.describe('Authentication API Tests', () => {
     console.log('Loaded Environment Variables:', Env);
   });
 
-  test('Register User', async ({ request }) => {
-    const uniqueEmail = `john.doe+${Date.now()}@example.com`;
-    const response = await request.post(`/api/auth/register`, {
-      ignoreHTTPSErrors: true,
-      data: {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: uniqueEmail,
-        password: 'password123',
-        confirmPassword: 'password123',
-      },
-    });
+  const baseURL = process.env['BASE_URL'] + ':' + process.env['PORT'];
 
-    // console.log(`Response Status: ${response.status()}`);
-    const responseBody = await response.json();
-    console.log('Response Body:', responseBody);
-    expect(response.ok()).toBeTruthy();
-    expect(responseBody.status).toBe(201);
-    expect(responseBody.message).toBe('User registered successfully.');
+  test.describe('Authentication API Tests', () => {
+    test('Register User', async ({ request }) => {
+      const uniqueEmail = `john.doe+${Date.now()}@example.com`;
+
+      const response = await request.post(baseURL + '/api/auth/register', {
+        ignoreHTTPSErrors: true,
+        data: {
+          firstName: 'John',
+          lastName: 'Doe',
+          email: uniqueEmail,
+          password: 'password123',
+          confirmPassword: 'password123',
+        },
+      });
+
+      // ตรวจสอบสถานะและข้อความตอบกลับ
+      expect(response.ok()).toBeTruthy();
+      const responseBody = await response.json();
+      expect(responseBody.status).toBe(201);
+      expect(responseBody.message).toBe('User registered successfully.');
+    });
   });
 
   // Uncomment and modify the following test as needed
