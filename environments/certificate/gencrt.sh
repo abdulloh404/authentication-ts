dbfe97f0313ea5a3ca358adcd3925b4cb1e356ab
Abdulloh <abdulloh.mukemm@gmail.com>
2025-01-03 11:24:38 +0700

    feat(play-write):init playwrite install

--- environments/certificate/gencrt.sh
+++ environments/certificate/gencrt.sh
@@ -1,6 +1,6 @@
 openssl req -x509 -nodes -new -sha256 -days 1024 -newkey rsa:4096 -keyout RootCA.key -out RootCA.pem -subj "/C=US/CN=Example-Root-CA"
 openssl x509 -outform pem -in RootCA.pem -out RootCA.crt
-openssl req -new -nodes -newkey rsa:4096 -keyout localhost.key -out localhost.csr -subj "/C=US/ST=YourState/L=YourCity/O=Example-Certificates/CN=localhost"
+openssl req -new -nodes -newkey rsa:4096 -keyout localhost.key -out localhost.csr -subj "/C=US/ST=YourState/L=YourCity/O=Example-Certificates/CN=localhost.com"
 openssl x509 -req -sha256 -days 1024 -in localhost.csr -CA RootCA.pem -CAkey RootCA.key -CAcreateserial -extfile extension.ext -out localhost.crt
 
 chmod 600 RootCA.key           
@@ -8,5 +8,5 @@ chmod 644 RootCA.crt
 chmod 600 localhost.key   
 chmod 644 localhost.crt    
 chmod 600 localhost.csr   
-
-ls -l RootCA.key RootCA.crt localhost.key localhost.crt localhost.csr
+# ls -l RootCA.key RootCA.crt localhost.key localhost.crt localhost.csr
+openssl x509 -in localhost.crt -text -noout
